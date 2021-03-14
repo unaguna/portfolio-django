b74094ce4766b579c045e10919fdca37fb2a1aec
unaguna <k.izumi.ysk@gmail.com>
2021-03-14 17:50:40 +0900

    💄記事一覧を製作物一覧へ変更

--- unaguna_p/cms/urls.py
+++ unaguna_p/cms/urls.py
@@ -4,5 +4,5 @@ from cms import views
 app_name = 'cms'
 urlpatterns = [
     # 製作物一覧
-    path('', views.product_list, name='product_list'),
+    path('products/', views.product_list, name='product_list'),
 ]
