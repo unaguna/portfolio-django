e3ca8a8259a1847917cff5bd7ae87504d70dff3f
unaguna <k.izumi.ysk@gmail.com>
2021-03-16 19:24:14 +0900

    🔧DB接続設定を開発と本番で分けられるよう修正

--- unaguna_p/unaguna_p/settings.py
+++ unaguna_p/unaguna_p/settings.py
@@ -77,13 +77,11 @@ WSGI_APPLICATION = 'unaguna_p.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+try:
+    DATABASES
+except NameError:
+    DATABASES = {
     }
-}
 
 
 # Password validation
