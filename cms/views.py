98eada8a912cf8986a2d0e32cf90a55eb6fa1141
unaguna <k.izumi.ysk@gmail.com>
2021-03-28 12:01:16 +0900

    🥅本文がない製作物記事を表示しないよう変更

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Product, Tag, Article
@@ -6,8 +7,16 @@ from .models import Product, Tag, Article
 def product_page(request, article_id):
     """記事（製作物）
     """
-    article = Article.objects.get(id=article_id)
-    product = Product.objects.get(article=article)
+    try:
+        article = Article.objects.get(id=article_id)
+        product = Product.objects.get(article=article)
+    except Exception:
+        raise Http404("The article doesn't exist.")
+
+    # 記事本文が公開されていないページは表示しない
+    if article.body is None or article.body == '':
+        raise Http404("The article has no body.")
+
     return render(request,
                   'cms/product.html',
                   {'product': product})
